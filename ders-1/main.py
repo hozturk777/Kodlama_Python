4a85f93154413ea5cb48691ce197e6f3504615b4
Hüseyin Öztürk <100473592+hozturk777@users.noreply.github.com>
2023-03-09 01:06:05 +0300

    ders-1

---
+++ ders-1/main.py
@@ -0,0 +1,116 @@
+print("kodlamaio")
+baslik = "Taşıt Kredisi"
+print("baslik") 
+#string => metinsel ifade 
+baslik = "İhtiyac Kredisi"
+print("baslik")
+
+#int , integer => tam sayı 
+vade = 36
+ekVade = 6
+vade2 ="35"
+
+#float , decimal , double 
+aylikOdeme = 200.50 
+
+#bool , boolean => true or false 
+yukselisteMİ = False
+
+#matematiksel operatörler (toplama)
+print(2+2)
+print(vade + 12)
+print(vade + ekVade)
+print(36 + 6)
+
+#matematiksel operatörler (Çıkarma)
+print(10-5)
+print(vade - 12)
+print(vade - ekVade)
+print(29 - 6)
+
+#matematiksel operatörler (Çarpma)
+
+print(22)
+print(vade * ekVade)
+print(10*4)
+
+#matematiksel operatörler (Bölme)
+print(8/2)
+print(vade / 2)
+print(vade / ekVade)
+print(40/4)
+
+
+yenivade = vade / 2
+fiyat = 130 
+indirimliFiyat = fiyat - 20
+
+print(yenivade)
+print(indirimliFiyat)
+
+
+#Mod Operatörü %
+print(8%2)
+print(vade % 2)
+print(vade % ekVade)
+print(40 % 4)
+
+
+
+#Selenium web otomasyon sistemidir. Framework gelişmiş otomatize testler. 
+
+
+#Mantıksal Operatörler ==
+print(40 == 4 )
+print(40 == 1 )
+print(40 == 40 )
+
+#Mantıksal Operatörler >
+print(40 > 4 )
+print(40 > 1 )
+print(40 > 40 )
+
+#Mantıksal Operatörler <
+
+print(40 < 4 )
+print(40 < 1 )
+print(40 < 40 )
+
+#Mantıksal Operatörler <=
+
+print(40 <= 4 )
+print(40 <= 1 )
+print(40 <= 40 )
+
+#Mantıksal Operatörler >=
+
+print(40 >= 4 )
+print(40 >= 1 )
+print(40 >= 40 )
+
+#ctrl + k c
+
+# #Mantıksal Operatörler >=
+# print(40 >= 4 )
+# print(40 >= 1 )
+# print(40 >= 40 )
+
+#Mantıksal Operatörler !=
+print(40 != 4 )
+print(40 != 1 )
+print(40 != 40 )
+
+# or and => veya 
+
+print (1 > 2 or 5 > 2 )
+print (1 > 2 and  5 > 2 )
+print (1 > 2 or  5 > 2 and 3 > 2  )
+
+if 2 < 3: #parantez yok indent(boşluk) var
+    print("deneme")
+elif 4 < 5:
+    print("deneme2")
+else: 
+    print("deneme3")
+
+    
