abf5b120e2638544cec530b19a8bcd2f0d5eeea6
Hüseyin Öztürk <100473592+hozturk777@users.noreply.github.com>
2023-03-10 00:30:19 +0300

    Array_Loop

---
+++ Array_Loop/main.py
@@ -0,0 +1,28 @@
+cars = ["bmw", "audi", "volvo"]
+
+for car in cars:
+    print(car)
+
+print(len(cars)) #lenght
+print(cars[0])
+
+for i in range(len(cars)):
+    print(cars[i])
+x = int(3)
+print(x)
+
+
+
+colors = ["red", "blue", "siyah"]
+for x in colors:
+    print("renK:"+x)
+print(len(colors))
+
+
+for x in range(len(colors)):
+    print(colors[x])
+
+for i in range(3, 10):
+    print("sayı:",i)
+for i in range(0, 10, 2):
+    print(i)
